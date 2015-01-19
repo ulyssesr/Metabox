a7ed6ffda305f473abc05d9b5c89d1f5f8e77754
Ulysses <ulyssesr@yahoo.com>
2015-01-19 13:04:09 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Metabox
+
+Meta Box is a WordPress plugin that creates a custom meta box within a post. The custom fields are available as shortcodes for display on templates or within posts.
---
+++ tfccpt/tfccpt.php
@@ -0,0 +1,173 @@
+<?php
+
+
+/**
+ * Plugin Name: TFC Custom Meta Box
+ * Plugin URI: http://uly.me
+ * Description: Creates a custom meta box for posts and pages.
+ * Version: 1.0
+ * Author: Ulysses Ronquillo
+ * Author URI: http://uly.me
+ * License: GPL2
+ */
+
+
+
+/**
+ * Adds a box to the main column on the Post and Page edit screens.
+ */
+
+function tfc_add_custom_meta_box() {
+
+	$screens = array( 'post', 'page' );
+
+	foreach ( $screens as $screen ) {
+
+		add_meta_box(
+
+			'tfc_section_id',
+			__( 'Custom Meta Box : Messages', 'tfc_textdomain' ),
+			'tfc_custom_meta_box_callback',	
+			$screen
+
+		);
+	}
+}
+
+add_action( 'add_meta_boxes', 'tfc_add_custom_meta_box' );
+
+
+/**
+ * Prints the box content.
+ * 
+ * @param WP_Post $post The object for the current post/page.
+ */
+
+function tfc_custom_meta_box_callback( $post ) {
+
+	// Add an nonce field so we can check for it later.
+	wp_nonce_field( 'tfc_custom_meta_box', 'tfc_custom_meta_box_nonce' );
+
+	/*
+	 * Use get_post_meta() to retrieve an existing value
+	 * from the database and use the value for the form.
+	 */
+
+	$message_speaker 		= get_post_meta( $post->ID, '_my_meta_value_speaker_key', true );
+	$message_date 			= get_post_meta( $post->ID, '_my_meta_value_date_key', true );
+	$message_length 		= get_post_meta( $post->ID, '_my_meta_value_length_key', true );
+
+	echo '<label style="text-align:left;;width:220px;float:left;padding-top:5px;" class="tfc_message" for="tfc_message">';
+	_e( 'Speaker: ', 'tfc_textdomain' );
+	echo '</label> ';
+	echo '<input type="text" id="tfc_message_speaker" name="tfc_message_speaker" value="' . esc_attr( $message_speaker ) . '" size="30" />';
+	echo '<br/>';	
+
+	echo '<label style="text-align:left;;width:220px;float:left;padding-top:5px;" class="tfc_message" for="tfc_message">';
+	_e( 'Date: (Format: Dec 20, 2014) ', 'tfc_textdomain' );
+	echo '</label> ';
+	echo '<input type="text" id="tfc_message_date" name="tfc_message_date" value="' . esc_attr( $message_date ) . '" size="30" />';
+	echo '<br/>';
+
+	echo '<label style="text-align:left;;width:220px;float:left;padding-top:5px;" class="tfc_message" for="tfc_message">';
+	_e( 'Length: (Format: 00:18:55) ', 'tfc_textdomain' );
+	echo '</label> ';
+	echo '<input type="text" id="tfc_message_length" name="tfc_message_length" value="' . esc_attr( $message_length ) . '" size="30" />';
+	echo '<br/><br/>';
+
+}
+
+/**
+ * When the post is saved, saves our custom data.
+ *
+ * @param int $post_id The ID of the post being saved.
+ */
+
+function tfc_save_meta_box_data( $post_id ) {
+
+	/*
+	 * We need to verify this came from our screen and with proper authorization,
+	 * because the save_post action can be triggered at other times.
+	 */
+
+	// Check if our nonce is set.
+	if ( ! isset( $_POST['tfc_custom_meta_box_nonce'] ) ) {
+		return;
+	}
+
+	// Verify that the nonce is valid.
+	if ( ! wp_verify_nonce( $_POST['tfc_custom_meta_box_nonce'], 'tfc_custom_meta_box' ) ) {
+		return;
+	}
+
+	// If this is an autosave, our form has not been submitted, so we don't want to do anything.
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+
+	// Check the user's permissions.
+	if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
+
+		if ( ! current_user_can( 'edit_page', $post_id ) ) {
+			return;
+		}
+
+	} else {
+
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+			return;
+		}
+	}
+
+	/* OK, it's safe for us to save the data now. */
+	
+	// Make sure that it is set.
+	if ( ! isset( $_POST['tfc_message_speaker'] ) ) { return; }
+	if ( ! isset( $_POST['tfc_message_date'] ) ) { return; }
+	if ( ! isset( $_POST['tfc_message_length'] ) ) { return; }
+
+	// Sanitize user input.
+	$my_tfc_message_speaker 	= sanitize_text_field( $_POST['tfc_message_speaker'] );
+	$my_tfc_message_date 			= sanitize_text_field( $_POST['tfc_message_date'] );
+	$my_tfc_message_length 		= sanitize_text_field( $_POST['tfc_message_length'] );
+
+	// Update the meta field in the database.
+	update_post_meta( $post_id, '_my_meta_value_speaker_key', $my_tfc_message_speaker );
+	update_post_meta( $post_id, '_my_meta_value_date_key', $my_tfc_message_date);
+	update_post_meta( $post_id, '_my_meta_value_length_key', $my_tfc_message_length);
+
+}
+
+add_action( 'save_post', 'tfc_save_meta_box_data' );
+
+
+
+// short codes!!!!
+
+function show_tfc_message_speaker($atts, $content = NULL) {
+
+	global $post;
+	extract (shortcode_atts(array('param' => '',),$atts));
+	$message_speaker 		= get_post_meta( $post->ID, '_my_meta_value_speaker_key', true );
+	return $message_speaker;
+}
+add_shortcode ('show_custom_speaker', 'show_tfc_message_speaker');
+
+function show_tfc_message_date($atts, $content = NULL) {
+
+	global $post;
+	extract (shortcode_atts(array('param' => '',),$atts));
+	$message_date 		= get_post_meta( $post->ID, '_my_meta_value_date_key', true );
+	return $message_date;
+}
+add_shortcode ('show_custom_date', 'show_tfc_message_date');
+
+function show_tfc_message_length($atts, $content = NULL) {
+
+	global $post;
+	extract (shortcode_atts(array('param' => '',),$atts));
+	$message_length 		= get_post_meta( $post->ID, '_my_meta_value_length_key', true );
+	return $message_length;
+}
+add_shortcode ('show_custom_length', 'show_tfc_message_length');
+
